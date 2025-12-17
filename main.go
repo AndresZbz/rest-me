e738b83b336e4c5420978609dc7185028d7b8c48
AndresZbz <juanandresbouzab@gmail.com>
2025-12-16 20:58:23 -0400

    moved some prints

--- main.go
+++ main.go
@@ -12,22 +12,25 @@ import (
 )
 
 func main() {
-	app := tview.NewApplication();
+	app := tview.NewApplication()
 
 	var selected_method string = "GET"
 
 	//api result
 	result := tview.NewTextArea().
-	SetPlaceholder("Result of the API will show here")
+		SetPlaceholder("Result of the API will show here")
 	result.SetBorder(true)
 	result.SetTitle("Response result")
-	
+
 	var form *tview.Form
 
 	form = tview.NewForm().
-		AddDropDown("Method", []string{"GET", "POST", "PUT"}, 0, 
+		AddDropDown("Method", []string{"GET", "POST", "PUT"}, 0,
 			func(option string, option_index int) {
 				selected_method = option
+				if option != "GET" {
+					result.SetText("Different methods than 'GET' will not work atm.", false)
+				}
 			}).
 		AddInputField("URL", "", 40, nil, nil).
 		AddButton("Send", func() {
@@ -39,7 +42,6 @@ func main() {
 				return
 			}
 
-
 			//client http
 			client := &http.Client{Timeout: 30 * time.Second}
 			var req *http.Request
@@ -50,9 +52,8 @@ func main() {
 			} else {
 				// POST / PUT todo: add body
 				req, err = http.NewRequest(selected_method, url, nil)
-				result.SetText(fmt.Sprintf("Different methods than 'GET' will not work atm. %v", err), false)
 			}
-			
+
 			if err != nil {
 				result.SetText(fmt.Sprintf("Error creating request: %v", err), false)
 				return
@@ -67,18 +68,18 @@ func main() {
 				request_info += fmt.Sprintf("  %s: %s\n", key, strings.Join(values, ", "))
 			}
 			result.SetText(request_info+"\nSending request...\n", false)
-			
+
 			app.ForceDraw() // Force UI update
 
 			// Send the request
 			resp, err := client.Do(req)
-			
+
 			var response_text string
 			if err != nil {
 				response_text = fmt.Sprintf("ERROR:\n%v", err)
 			} else {
 				defer resp.Body.Close()
-				
+
 				// Read response body
 				body, err := io.ReadAll(resp.Body)
 				if err != nil {
@@ -86,27 +87,32 @@ func main() {
 				} else {
 					// Format response
 					response_text = fmt.Sprintf(
-						"RESPONSE:\nStatus: %s %d\nHeaders:\n", 
-						resp.Status, 
+						"RESPONSE:\nStatus: %s %d\nHeaders:\n",
+						resp.Status,
 						resp.StatusCode,
 					)
-					
+
 					for key, values := range resp.Header {
 						response_text += fmt.Sprintf("  %s: %s\n", key, strings.Join(values, ", "))
 					}
-					
+
 					response_text += fmt.Sprintf("\nBody:\n%s", string(body))
 				}
 			}
-			
+
 			// Update result
 			current_text := result.GetText()
 			result.SetText(current_text+"\n"+response_text, false)
 
-		})
+		}).
+		AddButton("Clear", func() {
+			result.SetText("", false)
+		}).
+		SetButtonsAlign(tview.AlignLeft)
 
 	form.SetHorizontal(true)
 	form.SetBackgroundColor(tcell.ColorBlack)
+	form.SetBorder(false)
 
 	flex := tview.NewFlex().
 		SetDirection(tview.FlexRow).
@@ -114,8 +120,8 @@ func main() {
 		AddItem(result, 0, 1, true)
 
 	flex.SetBorder(true).
-	SetTitle("RestMe, API Rest Client").
-	SetTitleAlign(tview.AlignCenter)
+		SetTitle("RestMe, API Rest Client").
+		SetTitleAlign(tview.AlignCenter)
 
 	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
